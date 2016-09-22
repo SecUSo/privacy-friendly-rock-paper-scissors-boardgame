2f7919ac4dfb81ed5b0fe4ced2fed726f3e5c759
David Giessing <david.giessing93@gmail.com>
2016-09-22 09:27:22 +0200

    Fix for links in about page, typos

--- app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/activities/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/activities/AboutActivity.java
@@ -3,7 +3,9 @@ package org.secuso.privacyfriendlyrockpaperscissorsboardgame.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 import org.secuso.privacyfriendlyrockpaperscissorsboardgame.R;
 
@@ -12,7 +14,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        ((TextView)findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
         ActionBar ab = getSupportActionBar();
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
