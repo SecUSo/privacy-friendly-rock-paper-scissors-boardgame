93e2b2fd1461ac097110afc26930884f3194a3fa
Christopher Beckmann <chris.beckm@gmail.com>
2017-12-22 00:30:26 +0100

    Tutorial can now be access through the NavigationDrawer. TutorialDialog removed.

--- app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/activities/HomeActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/activities/HomeActivity.java
@@ -40,33 +40,7 @@ public class HomeActivity extends BaseActivity {
         }
 
         int index = mSharedPreferences.getInt("lastChosenPage", 0);
-        boolean welcomeScreenShown = mSharedPreferences.getBoolean("welcomeScreen",false);
-        if(!welcomeScreenShown){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(R.string.sWelcomeTitle);
-            builder.setMessage(R.string.sWelcome);
-            builder.setPositiveButton(R.string.sDialogHandOverOkButton, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    mSharedPreferences.edit().putBoolean("welcomeScreen",true).apply();
-                }
-            });
-            builder.setIcon(R.mipmap.ic_drawer);
-            builder.setNegativeButton(R.string.sHelp, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    goToNavigationItem(R.id.nav_help);
-                }
-            });
-            Dialog dialog=builder.create();
-            dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialogInterface) {
-                    mSharedPreferences.edit().putBoolean("welcomeScreen",true).apply();
-                }
-            });
-            dialog.show();
-        }
+
         mViewPager.setCurrentItem(index);
         mArrowLeft = (ImageView) findViewById(R.id.arrow_left);
         mArrowRight = (ImageView) findViewById(R.id.arrow_right);
