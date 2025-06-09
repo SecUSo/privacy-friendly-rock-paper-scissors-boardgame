2631e47dc83ca3fb4be5a12418145fe6d661072f
Patrick Schneider <urulu@student.kit.edu>
2025-06-09 17:45:14 +0200

    Fixes copy-paste error regarding package

--- app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/GoodbyeGoogleHelper.kt
+++ app/src/main/java/org/secuso/privacyfriendlyrockpaperscissorsboardgame/GoodbyeGoogleHelper.kt
@@ -1,4 +1,4 @@
-package org.secuso.privacyfriendlyboardgameclock
+package org.secuso.privacyfriendlyrockpaperscissorsboardgame;
 
 import android.content.Context
 import android.graphics.text.LineBreaker
